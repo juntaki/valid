f52202091f6e3e8d12dcda7686848117ec263ead
juntaki <me@juntaki.com>
2021-09-08 02:35:57 +0900

    fix

--- id.go
+++ id.go
@@ -3,93 +3,140 @@ package valid
 import (
 	"crypto/rand"
 	"encoding/base32"
+	"encoding/binary"
 	"fmt"
 	"time"
 )
 
 var wordSafeBase32 = base32.NewEncoding("23456789CFGHJMPQRVWXcfghjmpqrvwx").WithPadding(base32.NoPadding)
 
-type IDSource struct {
-	RandomByteLength int
-	Checksum         bool
-	Sortable         bool
-	InitialTime      time.Time
+type Source interface {
+	WithChecksum() Source
+	WithTimestamp() Source
+	WithTimestampStartAt(initialTime time.Time) Source
+	Generate() string
+	Timestamp(id string) time.Time
+	IsValid(id string) bool
 }
 
-func (s IDSource) checksumByteLength() int {
-	if s.Checksum {
-		return 1
+type baseSource struct {
+	randomByteLength int
+	checksum         bool
+	timestamp        bool
+	initialTime      time.Time
+}
+
+func (s baseSource) WithChecksum() Source {
+	s.checksum = true
+	return s
+}
+
+func (s baseSource) WithTimestamp() Source {
+	s.timestamp = true
+	return s
+}
+
+func (s baseSource) WithTimestampStartAt(initialTime time.Time) Source {
+	s.timestamp = true
+	s.initialTime = initialTime
+	return s
+}
+
+func NewSource(randomByteLength int) Source {
+	if randomByteLength < 1 {
+		panic("random byte length < 1")
 	}
-	return 0
-}
-
-func (s IDSource) sortableByteLength() int {
-	if s.Sortable {
-		return 6
+	return baseSource{
+		randomByteLength: randomByteLength,
+		checksum:         false,
+		timestamp:        false,
+		initialTime:      time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 	}
-	return 0
 }
 
-func (s IDSource) totalByteLength() int {
-	return s.sortableByteLength() + s.RandomByteLength + s.checksumByteLength()
-}
+func (s baseSource) Generate() string {
+	b := make([]byte, s.byteLength())
 
-func (s IDSource) randomByteOffset() int {
-	return s.sortableByteLength() + s.RandomByteLength
-}
-
-var DefaultSource = IDSource{
-	RandomByteLength: 16, // 128bit
-	Checksum:         true,
-	Sortable:         true,
-	InitialTime:      time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-}
-
-func (s IDSource) Generate() string {
-	b := make([]byte, s.totalByteLength())
-
+	cursor := 0
 	// Timestamp
-	if s.Sortable {
-		timestamp := b[:s.sortableByteLength()]
-		// 40bit timestamp
-		ts := (time.Now().UnixNano() - s.InitialTime.UnixNano()) >> 20 & ((1 << 40) - 1)
+	if s.timestamp {
+		// 40bit timestamp ~ Millisecond
+		ts := (time.Now().UnixNano() - s.initialTime.UnixNano()) >> 20 & ((1 << 40) - 1)
 
-		timestamp[0] = byte(ts >> 32)
-		timestamp[1] = byte(ts >> 24)
-		timestamp[2] = byte(ts >> 16)
-		timestamp[3] = byte(ts >> 8)
-		timestamp[4] = byte(ts)
+		b[0] = byte(ts >> 32)
+		b[1] = byte(ts >> 24)
+		b[2] = byte(ts >> 16)
+		b[3] = byte(ts >> 8)
+		b[4] = byte(ts)
+		cursor = 5
 	}
 
 	// Random value
-	random := b[s.sortableByteLength():s.randomByteOffset()]
-	_, err := rand.Reader.Read(random)
+	_, err := rand.Read(b[cursor : cursor+s.randomByteLength])
 	if err != nil {
 		panic(fmt.Errorf("failed to read random value"))
 	}
+	cursor = cursor + s.randomByteLength
 
-	// Checksum
-	if s.Checksum {
-		checksum := b[s.randomByteOffset():]
-		for _, b := range b[:s.randomByteOffset()] {
-			checksum[0] = checksum[0] ^ b
+	// checksum
+	if s.checksum {
+		for _, bb := range b[:cursor] {
+			b[cursor] = b[cursor] ^ bb
 		}
 	}
 
 	return wordSafeBase32.EncodeToString(b)
 }
 
-func (s IDSource) IsValid(id string) bool {
+func (s baseSource) Timestamp(id string) time.Time {
+	if !s.timestamp {
+		return time.Time{}
+	}
+	b, err := wordSafeBase32.DecodeString(id)
+	if err != nil {
+		return time.Time{}
+	}
+
+	ts := make([]byte, 8)
+	ts[3] = b[0]
+	ts[4] = b[1]
+	ts[5] = b[2]
+	ts[6] = b[3]
+	ts[7] = b[4]
+
+	val := binary.BigEndian.Uint64(ts) << 20
+	return time.Unix(0, s.initialTime.UnixNano()+int64(val))
+}
+
+func (s baseSource) byteLength() int {
+	bLen := s.randomByteLength
+	if s.timestamp {
+		bLen += 5
+	}
+	if s.checksum {
+		bLen += 1
+	}
+	return bLen
+}
+
+func (s baseSource) IsValid(id string) bool {
+	if !s.checksum {
+		return false // No checksum
+	}
+
 	b, err := wordSafeBase32.DecodeString(id)
 	if err != nil {
 		return false
 	}
 
-	var checksum byte
-	checksumPart := b[s.randomByteOffset():]
-	for _, b := range b[:s.randomByteOffset()] {
-		checksum = checksum ^ b
+	if len(b) != s.byteLength() {
+		return false
 	}
 
-	return checksum == checksumPart[0]
+	var checksum byte
+	for _, bb := range b {
+		checksum = checksum ^ bb
+	}
+
+	return checksum == byte(0)
 }
--- id_test.go
+++ id_test.go
@@ -1,33 +1,44 @@
 package valid
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
+var ds = NewSource(16).WithTimestamp().WithChecksum()
+
 func Test_generateID(t *testing.T) {
-	for i := 0; i < 100; i++ {
-		got := DefaultSource.Generate()
-		fmt.Println(got, len(got))
-		time.Sleep(time.Millisecond)
-		if !DefaultSource.IsValid(got) {
+	for i := 1; i < 100; i++ {
+		ds = NewSource(i).WithTimestamp().WithChecksum()
+		got := ds.Generate()
+		if time.Now().Sub(ds.Timestamp(got)).Milliseconds() > 1 {
 			t.Fatal("invalid")
 		}
+		if !ds.IsValid(got) {
+			t.Fatal("invalid")
+		}
+		time.Sleep(time.Millisecond)
+	}
+}
+
+func Test_validateID(t *testing.T) {
+	bad := "2X75jjvg2zzzGWq9JHjm88PHvR49gQX2FGqH6"
+	if ds.IsValid(bad) {
+		t.Fatal("invalid")
 	}
 }
 
 func Benchmark_generateID(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		DefaultSource.Generate()
+		ds.Generate()
 	}
 }
 
 func Benchmark_validateID(b *testing.B) {
-	got := DefaultSource.Generate()
+	got := ds.Generate()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		DefaultSource.IsValid(got)
+		ds.IsValid(got)
 	}
 }
