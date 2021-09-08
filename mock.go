47df5039b971072bfd043e818cd5a2597d92c733
juntaki <me@juntaki.com>
2021-09-09 02:35:02 +0900

    fix

--- mock.go
+++ mock.go
@@ -4,56 +4,57 @@ import (
 	"time"
 )
 
-type MockSource struct {
-	RandomByteLength int
-	UseChecksum      bool
-	UseTimestamp     bool
-	DummyTimestamp   time.Time
+type mockSource struct {
+	randomByteLength int
+	useChecksum      bool
+	useTimestamp     bool
+	dummyTimestamp   time.Time
 }
 
-func NewMockSource(randomByteLength int) *MockSource {
-	return &MockSource{
-		RandomByteLength: randomByteLength,
-		UseChecksum:      false,
-		UseTimestamp:     false,
-		DummyTimestamp:   time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+func NewMockSource(randomByteLength int) Source {
+	return &mockSource{
+		randomByteLength: randomByteLength,
+		useChecksum:      false,
+		useTimestamp:     false,
+		dummyTimestamp:   time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 	}
 }
 
-func (m MockSource) WithChecksum() Source {
-	m.UseChecksum = true
+func (m mockSource) WithChecksum() Source {
+	m.useChecksum = true
 	return m
 }
 
-func (m MockSource) WithTimestamp() Source {
-	m.UseTimestamp = true
+func (m mockSource) WithTimestamp() Source {
+	m.useTimestamp = true
 	return m
 }
 
-func (m MockSource) WithTimestampStartAt(initialTime time.Time) Source {
-	m.UseTimestamp = true
+func (m mockSource) WithTimestampStartAt(initialTime time.Time) Source {
+	m.useTimestamp = true
+	m.dummyTimestamp = initialTime
 	return m
 }
 
-func (m MockSource) Generate() string {
+func (m mockSource) Generate() string {
 	b := make([]byte, m.byteLength())
 	return wordSafeBase32.EncodeToString(b)
 }
 
-func (m MockSource) Timestamp(id string) time.Time {
-	return m.DummyTimestamp
+func (m mockSource) Timestamp(id string) time.Time {
+	return m.dummyTimestamp
 }
 
-func (m MockSource) IsValid(id string) bool {
+func (m mockSource) IsValid(id string) bool {
 	return true
 }
 
-func (m MockSource) byteLength() int {
-	l := m.RandomByteLength
-	if m.UseTimestamp {
+func (m mockSource) byteLength() int {
+	l := m.randomByteLength
+	if m.useTimestamp {
 		l += 5
 	}
-	if m.UseChecksum {
+	if m.useChecksum {
 		l++
 	}
 	return l
